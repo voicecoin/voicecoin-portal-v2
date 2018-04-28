c67ea4575f0820eea3232d68314970e7de6abaa3
Yuancheng Zhang <old.far.hot@gmail.com>
2018-04-27 21:55:18 -0500

    tranlate special

--- js/i18.js
+++ js/i18.js
@@ -19,6 +19,13 @@ var app = new Vue({
 					sub2: 'an universal bridge between chatbot and IoT.',
 					sub3: 'Voicecoin is the money of Voiceweb.',
 					buy: 'Buy Token',
+				},
+				spe: {
+					h2: 'Ask what you need',
+					p1: 'Escape the screen and endless scrolling, get what you’re looking for by just ASKING.',
+					p2: 'By 2020, Morgan Stanley predicts 75 billion IoT devices will be connected. We are going to step into the post mobile era, next generation internet is voice Internet. People will talk to the computer like the way we talk to people—minus the sassiness!',
+					more: 'Read More',
+					faq: 'FAQ'
 				}
 			},
 			zh: {
@@ -33,10 +40,17 @@ var app = new Vue({
 				},
 				wel: {
 					title: 'VOICECOIN',
-					sub1: '语音币，基于区块链技术的分布式智能语音网络',
+					sub1: '语音币，去中心化的语音万维网',
 					sub2: '机器人与物联网之间的桥梁',
 					sub3: '语音币是语音网络的通用货币',
 					buy: '购 买',
+				},
+				spe: {
+					h2: '答你所想 不负众望',
+					p1: ' ',
+					p2: '摩根士丹利预计到2020年会有750亿台IoT设备联网。我们将进入后移动时代，而下一代互联网是语音互联网。语音链就在现在：极速而至。从此：人机交流会像人与人之间的交流一样畅快淋漓！有我们，世界为你们而改变！',
+					more: '更多',
+					faq: 'FAQ'
 				}
 			}
 		}
